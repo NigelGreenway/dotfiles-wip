243118234473dca3d6ea5e1162d8d0deee95cd91
Nigel Greenway <nigel_greenway@me.com>
2014-07-29 08:23:55 +0100

    Remove `php cs fixer`

--- .dotfiles/.lang/php-cs-fixer.sh
+++
@@ -1,8 +0,0 @@
-$fixer='php-cs-fixer fix --fixers=linefeed,trailing_spaces,unused_use,php_closing_tag,short_tag,return,visibility,braces,phpdoc_params,eof_ending,psr0,controls_spaces,elseif'
-
-function phpCSFixer {
-	for f in `find ./src -name *.php`
-	do
-		$fixer $f
-	done
-}
